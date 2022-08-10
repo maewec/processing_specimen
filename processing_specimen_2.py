f2f9abdee0814cb1da76e8bcb26bbb0249279cdf
maewec <nemtsev@numr.org>
2022-08-10 13:51:19 +0300

    Добавил копирование объекта при сглаживании фронтов и переделал константы названий столбцов

--- processing_specimen_2.py
+++ processing_specimen_2.py
@@ -7,6 +7,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import copy
 
 class Specimen:
     def __init__(self, list_fronts, list_names=None):
@@ -25,11 +26,16 @@ class Specimen:
         for i in range(len(list_fronts)):
             self.table[self.list_names[i]] = self.__read_file(self.list_fronts[i])
 
+    NAMES_COUNTUR = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6']
+    NAME_INDEX = ['ang']
+    NAMES_COORD = ['ang', 'rad', 'z']
+    NAME_DROP = ['node']
+    NAMES_COLUMNS = NAME_DROP + NAMES_COORD + NAMES_COUNTUR
 
     def __read_file(self, path):
-        names = ('node', 'ang', 'rad', 'z', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6')
-        index_col = 'ang'
-        drop = 'node'
+        names = Specimen.NAMES_COLUMNS
+        index_col = Specimen.NAME_INDEX
+        drop = Specimen.NAME_DROP
         return pd.read_table(path, delim_whitespace=True, names=names,
                 index_col=index_col).drop(columns=drop)
 
@@ -52,4 +58,35 @@ class Specimen:
         for name in self.table:
             self.table[name][cont].plot(label=name)
         plt.legend()
+
+    @staticmethod
+    def __moving_average(array, num=5):
+        """Сглаживание фронтов с помощью скользящего среднего"""
+        res = np.zeros_like(array)
+        delta_1 = int(num/2)
+        delta_2 = num - delta_1
+        length = len(array)
+        for i in range(length):
+            minn = i-delta_1
+            maxx = i+delta_2
+            inc = 1
+            if maxx <= length - 1:
+                inc = 1
+                ind = np.arange(minn, maxx)
+            else:
+                maxx -= length
+                inc = -1
+                ind = np.concatenate((np.arange(minn, length), np.arange(0, maxx)))
+            summ = array[ind].sum()
+            res[i] = summ / num
+        return res
+
+    def copy_smooth(self, num):
+        """Создает копию объекта со сглаженными фронтами"""
+        obj_copy = copy.deepcopy(self)
+        for name in obj_copy.table:
+            for contour in Specimen.NAMES_COUNTUR:
+                arr = np.array(obj_copy.table[name][contour])
+                obj_copy.table[name][contour] = self.__moving_average(arr, num=num)
+        return obj_copy
 
