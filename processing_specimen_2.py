8308d568c1908ccbc619bbea43531ee8d3ce6632
maewec <nemtsev@numr.org>
2023-01-27 10:17:34 +0300

    Убрал у сплайна из настроек периодичность, вместо этого расширил массив для задания сплайна слева и справа

--- processing_specimen_2.py
+++ processing_specimen_2.py
@@ -1237,10 +1237,10 @@ class CurveFrontsInterp:
         i = 0
         for curv in self.curves:
             curv_neutral = curv.convert_neutral(num_angs=num_angs)
-            curv_neutral_rad_arr[i,:] = curv_neutral.rads[:-1]
+            curv_neutral_rad_arr[i,:] = curv_neutral.rads_initial
             rads4ang[i] = curv_neutral.rad_from_ang(ang)
             i += 1
-        angs = np.degrees(curv_neutral.angs[:-1])
+        angs = np.degrees(curv_neutral.angs_initial)
         rad_new = np.zeros(num_angs)
         for i in range(num_angs):
             rad_new[i] = np.interp(rad, rads4ang, curv_neutral_rad_arr[:, i])
@@ -1249,8 +1249,21 @@ class CurveFrontsInterp:
     def insert_dop_form(self, list_df, angs_arr):
         """Добавить на существующие кривые фронтов врезку с новыми радиусами и углами,
         list_df - полученные таблицы, в которых определен рост циклов
+        Расположение таблиц в листе по часовой стрелке
         angs_arr - вектор углов, которые остаются от исходной кривой"""
-        ang_middle = []
+        # у всех таблиц индексы одинаковые, начинаем проходить по одному
+        for index in list_df[0].index:
+            # таблица в которой собрана одна строка других таблиц
+            df_row = pd.DataFrame(columns=['rad', 'ang'])
+            for df in list_df:
+                df_row = df_row.append(df.loc[index, ['rad', 'ang']])
+            rad_0 = df_row.iloc[0]['rad']
+            ang_0 = df_row.iloc[-1]['ang']
+            curve_0 = self.search_curve(rad_0, ang_0)
+            rad_1 = df_row.iloc[0]['rad']
+            ang_1 = df_row.iloc[-1]['ang']
+            curve_1 = self.search_curve(rad_1, ang_1)
+
 
 
 
@@ -1267,10 +1280,14 @@ class CurveFront8point:
             self.angs = np.arange(0, 2*np.pi, 2*np.pi/l)
         else:
             self.angs = np.radians(angs)
-        self.rads = np.concatenate((self.rads, self.rads[:1]), axis=0)
-        self.angs = np.concatenate((self.angs, self.angs[:1]+2*np.pi), axis=0)
+        self.rads_initial = self.rads
+        self.angs_initial = self.angs
+        # расширение пределов массива для плавного соединения сплайна
+        self.rads = np.concatenate((self.rads, self.rads, self.rads))
+        self.angs = np.concatenate((self.angs-2*np.pi, self.angs,
+                                    self.angs+2*np.pi))
 
-        self.__tck = interpolate.splrep(self.angs, self.rads, per=2*np.pi, k=3)
+        self.__tck = interpolate.splrep(self.angs, self.rads, k=3)
 
     def plot_ax(self, ax, color='k', alpha=0.3):
         angs = np.linspace(0, 2*np.pi, 1000)
