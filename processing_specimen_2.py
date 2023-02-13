636c996294f9a04ab7a6aefbb083adc7163234af
maewec <nemtsev@numr.org>
2023-02-13 16:35:28 +0300

    Добавил класс ImageSpecimen и изменил geom_plot_contour для отображения подложки из изображения излома

--- processing_specimen_2.py
+++ processing_specimen_2.py
@@ -16,6 +16,7 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 from scipy.stats import linregress
 import itertools
 from scipy import interpolate
+from PIL import Image
 
 from padmne.pdnforcrack.forcrack import OneCycle
 
@@ -162,12 +163,13 @@ class Specimen:
         return obj_copy
 
     def plot_geom_front(self, cont=None, plot_rad_obr=True, plot_rad_def=True,
-                        dir_theta_null='S', plot_curve_front_data=True):
+                        dir_theta_null='S', plot_curve_front_data=True,
+                        save_plot_with_image=None):
         """Печать геометрии фронтов трещины с нанесенными значениями КИН"""
 
         cont_dict = self.__cont_dict(cont)
         
-        fig = plt.figure(figsize=(15, 15))
+        fig = plt.figure(figsize=(10, 10), dpi=250)
         ax = fig.add_subplot(projection='polar')
 
         rad_max = 0
@@ -225,6 +227,10 @@ class Specimen:
         if plot_curve_front_data:
             if self.curve_front_data:
                 ax = self.curve_front_data.plot_ax_initial(ax)
+
+        # если есть объект с изображением поверхности излома, то сохранить его
+        if isinstance(save_plot_with_image, ImageSpecimen):
+            save_plot_with_image.save_fig(fig)
 
     def __sdvig(self, rad):
         div = 100
@@ -1320,4 +1326,78 @@ class CurveFront8point:
         angs = angs_arr
         rads = self.rad_from_ang(angs)
         return CurveFront8point(rads, angs)
+
+
+
+class ImageSpecimen:
+    """Описание изображения поверхности образца для подложки графика"""
+
+    count = 0
+
+    def __init__(self, pathfile, point_center_pix, point_center_mm,
+                 point_rot_pix, point_rot_mm, angle=0):
+        """Parameter:
+        pathfile - путь до файла
+        point_center_pix - координаты центра в пикселях
+        point_center_mm - координаты центра в мм
+        point_rot_pix - координаты второй точки в пикселях
+        point_rot_mm - координаты второй точки в мм
+        angle - дополнительный поворот в градусах"""
+
+        self.pathfile = pathfile
+        self.pcp = np.array(point_center_pix)
+        self.pcm = np.array(point_center_mm)
+        self.prp = np.array(point_rot_pix)
+        self.prm = np.array(point_rot_mm)
+        self.angle = angle
+
+        self.img = Image.open(pathfile)
+        # в одном мм пикселей
+        self.pix_in_mm = np.abs((self.pcp - self.prp) / (self.pcm - self.prm))[0]
+        self.name = os.path.splitext(os.path.basename(self.pathfile))[0]
+
+    def save_fig(self, fig):
+        ax = fig.axes[0]
+        # прозначный фон для графика
+        ax.patch.set_alpha(0)
+
+        # пикселей в мм графика
+        l = 1
+        pix_graph = (ax.transData.transform([0, 0]) - ax.transData.transform([0, l]))[1]
+        pix_graph = np.abs(pix_graph)
+        k_resize = pix_graph / self.pix_in_mm
+
+        # новые размеры для изображения
+        new_x = int(round(self.img.size[0] * k_resize, 0))
+        new_y = int(round(self.img.size[1] * k_resize, 0))
+        img = self.img.resize((new_x, new_y), resample=Image.BILINEAR)
+
+        # выравниваем горизонт по второй точке
+        dif = self.pcp - self.prp
+        ang = np.degrees(np.arctan(dif[1]/dif[0]))
+        # и добавляем ручной поворот
+        ang = ang + self.angle
+
+        # находим координаты в пикселях нулевой точки графика
+        # и пересчитываем новые координаты центральной точки изображения
+        # в пикселях с учетом ресайза
+        point_center_graph = ax.transData.transform([0, 0])
+        point_center_graph = np.array(np.around(point_center_graph, 0), dtype=int)
+        pcp_resize = self.pcp * k_resize
+        pcp_resize = np.array(np.around(pcp_resize, 0), dtype=int)
+        sdvig = point_center_graph - pcp_resize
+
+        # поворот относительно центральной точки
+        img = img.rotate(angle=ang, expand=False, center=tuple(pcp_resize),
+                         resample=Image.BILINEAR, fillcolor='white')
+        # внедрение изображения в график
+        fig.figimage(img, sdvig[0], sdvig[1], zorder=-1, resize=False, origin='upper')
+
+        name = '{}_{:2.0f}.png'.format(self.name, ImageSpecimen.count)
+        ImageSpecimen.count += 1
+        fig.savefig(name)
+
+
+
+
 
